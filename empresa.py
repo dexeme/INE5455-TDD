59bbbd5cfa1e58a688b2536164cf99710c3a545a
dexeme <dexe.me12@gmail.com>
2026-05-12 11:02:18 -0300

    Adiciona Teste test_inclui_projeto_na_empresa

--- empresa.py
+++ empresa.py
@@ -1,7 +1,10 @@
 from funcionario import Funcionario
+from projeto import Projeto
+
 
 class Empresa():
     funcionarios: list[Funcionario]
+    projetos: list[Projeto]
 
     def __init__(self, nome: str):
         nome = nome.strip()
@@ -10,12 +13,17 @@ class Empresa():
         self.nome = nome
 
         self.funcionarios = []
+        self.projetos = []
 
     def adiciona_funcionario(self, funcionario: Funcionario):
         if funcionario in self.funcionarios:
             raise ErroFuncionarioRepetido(f"{funcionario.nome} já está na lista de funcionários da empresa {self.nome}.")
         self.funcionarios.append(funcionario)
 
+    def adiciona_projeto(self, projeto: Projeto):
+        if projeto in self.funcionarios:
+            raise ErroProjetoRepetido(f"{projeto.nome} já está na lista de projetos da empresa {self.nome}.")
+        self.projetos.append(projeto)
 
 class ErroFuncionarioRepetido(Exception):
     pass
