9cde323995f9a38cff760840e4555b607918e5ce
dexeme <dexe.me12@gmail.com>
2026-05-12 10:22:14 -0300

    Cod. Prod. test_cria_empresa_nome_vazio

--- empresa.py
+++ empresa.py
@@ -1,3 +1,6 @@
 class Empresa():
     def __init__(self, nome: str):
+        nome = nome.strip()
+        if not nome:
+            raise ValueError("Empresa com nome vazio.")
         self.nome = nome
