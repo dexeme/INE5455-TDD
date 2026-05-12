5f479e5fcd841e3d6c5f6d0debf0225d44898c11
dexeme <dexe.me12@gmail.com>
2026-05-12 10:57:54 -0300

    Cod. Prod. test_cria_projeto_nome_vazio

--- projeto.py
+++ projeto.py
@@ -3,4 +3,7 @@ class Projeto:
     nome: str
 
     def __init__(self, nome: str):
+        nome = nome.strip()
+        if not nome:
+            raise ValueError("Projeto com nome vazio.")
         self.nome = nome
